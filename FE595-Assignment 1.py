5a3185a1fb723b8ad7a17f99680a435f1ddf7be9
apadman2 <70728634+apadman2@users.noreply.github.com>
2020-09-27 23:21:49 -0400

    Update FE595-Assignment 1.py

--- FE595-Assignment 1.py
+++ FE595-Assignment 1.py
@@ -5,11 +5,12 @@ import numpy as np
 x = np.arange(0, 2*np.pi, 0.01)
 y = np.sin(x)
 z = np.cos(x)
+t = np.tan(x)
 
-plt.plot(x, y, x, z)
+plt.plot(x, y, x, z, x, t)
 plt.xlabel('X-values from 0 to 2 pi')
-plt.ylabel('sin(x), cos(x)')
-plt.legend(['sin(x)', 'cos(x)'])
-plt.title('Plot of sin(x) and cos(x) for one period')
+plt.ylabel('sin(x), cos(x), tan(x)')
+plt.legend(['sin(x)', 'cos(x)', 'tan(x)'])
+plt.title('Plot of sin(x), cos(x) and tan(x) for one period')
 plt.show()
 
